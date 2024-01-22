1a2a8d9db88d6c497d44c4bc30f9da718c9b68ed
KevinPasteur <kevin.pasteur@icloud.com>
2024-01-22 14:27:35 +0100

    Add layout page

---
+++ src/app/layout/layout.page.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { book, people, personCircle } from "ionicons/icons";
+import { addIcons } from 'ionicons';
+
+// Custom type that represent a tab data.
+declare type PageTab = {
+  title: string; // The title of the tab in the tab bar
+  icon: string; // The icon of the tab in the tab bar
+  path: string; // The route's path of the tab to display
+};
+
+@Component({
+  selector: 'app-layout',
+  templateUrl: './layout.page.html',
+  styleUrls: ['./layout.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule]
+})
+export class LayoutPage {
+  tabs: PageTab[];
+
+  constructor() {
+    this.tabs = [
+      { title: "Bibliothèque", icon: book, path: "library" },
+      { title: "Communauté", icon: people, path: "community" },
+    ];
+    addIcons({personCircle});
+  }
+
+  
+}
