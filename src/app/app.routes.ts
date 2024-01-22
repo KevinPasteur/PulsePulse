3276fc5a1992ed469c6a7b77faf3f4cf542db6c3
KevinPasteur <kevin.pasteur@icloud.com>
2024-01-22 14:26:27 +0100

    Add routes for layout, community, library and login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,6 @@
 import { Routes } from '@angular/router';
+import { onlyAuthenticated } from "./security/only-authenticated.guard";
+
 
 export const routes: Routes = [
   {
@@ -10,4 +12,27 @@ export const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full',
   },
+  {
+    path: '',
+    loadComponent: () =>
+      import("./layout/layout.page").then((m) => m.LayoutPage),
+      //canActivate: [onlyAuthenticated],
+      children: [
+      {
+        path: 'community',
+        loadComponent: () => import('./layout/community/community.page').then( m => m.CommunityPage)
+      },
+      {
+        path: 'library',
+        loadComponent: () => import('./layout/library/library.page').then( m => m.LibraryPage),
+        
+      },
+    ]
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./auth/login/login.page').then( m => m.LoginPage)
+  },
+  
+  
 ];
