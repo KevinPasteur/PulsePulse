c463e57dfdfefedae581e12e8cd940ce7d8767b4
KevinPasteur <kevin.pasteur@icloud.com>
2024-01-22 14:28:18 +0100

    Add library page

---
+++ src/app/layout/library/library.page.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { heart, add, playCircleOutline } from "ionicons/icons";
+import { addIcons } from 'ionicons';
+
+@Component({
+  selector: 'app-library',
+  templateUrl: './library.page.html',
+  styleUrls: ['./library.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule]
+})
+export class LibraryPage {
+
+  selectedSegment: string = 'workouts';
+
+  constructor(private router: Router) {
+    addIcons({heart, add, playCircleOutline});
+  }
+
+  segmentChanged(event: any) {
+    const selectedValue = event.detail.value;
+
+  }
+
+}
