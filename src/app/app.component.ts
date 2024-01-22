377333a1a285249bc46ae4ac2691f134afefa8be
Kévin <kevin.pasteur@icloud.com>
2024-01-22 18:02:55 +0100

    Add storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
-
+import { Storage } from "@ionic/storage-angular";
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -8,5 +8,7 @@ import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
   imports: [IonApp, IonRouterOutlet],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(storage: Storage) {
+    storage.create();
+  }
 }
