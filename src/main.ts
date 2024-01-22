8acb142bc33410e3f5ae8d625a82556bbb8ee0cd
Kévin <kevin.pasteur@icloud.com>
2024-01-22 18:01:50 +0100

    Import storage angular

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
@@ -8,6 +8,7 @@ import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
 
 import { provideHttpClient } from "@angular/common/http";
+import { IonicStorageModule } from "@ionic/storage-angular";
 
 if (environment.production) {
   enableProdMode();
@@ -19,5 +20,6 @@ bootstrapApplication(AppComponent, {
     provideIonicAngular(),
     provideRouter(routes),
     provideHttpClient(),
+    importProvidersFrom(IonicStorageModule.forRoot()),
   ],
 });
