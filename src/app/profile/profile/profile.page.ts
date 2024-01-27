ef0143e39924523f7b395ab15c0add391937fca2
Kévin <kevin.pasteur@icloud.com>
2024-01-27 15:24:44 +0100

    Update profile page

--- src/app/profile/profile/profile.page.ts
+++ src/app/profile/profile/profile.page.ts
@@ -3,29 +3,30 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { NavController } from '@ionic/angular';
-import { AuthService } from "src/app/security/auth.service";
-import { Router } from "@angular/router";
+import { AuthService } from 'src/app/security/auth.service';
+import { Router } from '@angular/router';
+
+import { User } from 'src/app/security/user.model';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.page.html',
   styleUrls: ['./profile.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule],
 })
 export class ProfilePage {
-
-  constructor(
-    private auth: AuthService,
-    private router: Router)
-  {}
-
-
-
+  user: any;
+  constructor(private auth: AuthService, private router: Router) {
+    this.auth.getUser$().subscribe((user) => {
+      this.user = user;
+      console.log(this.user);
+    });
+  }
 
   logOut() {
-    console.log("logging out...");
+    console.log('logging out...');
     this.auth.logOut();
-    this.router.navigateByUrl("/login");
+    this.router.navigateByUrl('/login');
   }
 }
