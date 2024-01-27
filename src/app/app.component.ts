8df707175abaf33d7e82e7f322977328312cd1cb
Kévin <kevin.pasteur@icloud.com>
2024-01-27 15:22:09 +0100

    Add new routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
-import { Storage } from "@ionic/storage-angular";
+import { Storage } from '@ionic/storage-angular';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
-import { onlyAuthenticated } from "./security/only-authenticated.guard";
-
+import { onlyAuthenticated } from './security/only-authenticated.guard';
 
 export const routes: Routes = [
   {
@@ -15,33 +14,50 @@ export const routes: Routes = [
   {
     path: '',
     loadComponent: () =>
-      import("./layout/layout.page").then((m) => m.LayoutPage),
-      // canActivate: [onlyAuthenticated],
-      children: [
+      import('./layout/layout.page').then((m) => m.LayoutPage),
+    // canActivate: [onlyAuthenticated],
+    children: [
       {
         path: 'community',
-        loadComponent: () => import('./layout/community/community.page').then( m => m.CommunityPage)
+        loadComponent: () =>
+          import('./layout/community/community.page').then(
+            (m) => m.CommunityPage
+          ),
       },
       {
         path: 'library',
-        loadComponent: () => import('./layout/library/library.page').then( m => m.LibraryPage),
-
+        loadComponent: () =>
+          import('./layout/library/library.page').then((m) => m.LibraryPage),
       },
-    ]
+    ],
   },
   {
     path: 'login',
-    loadComponent: () => import('./auth/login/login.page').then( m => m.LoginPage)
+    loadComponent: () =>
+      import('./auth/login/login.page').then((m) => m.LoginPage),
   },
   {
     path: 'profile',
-    loadComponent: () => import('./profile/profile/profile.page').then( m => m.ProfilePage)
+    loadComponent: () =>
+      import('./profile/profile/profile.page').then((m) => m.ProfilePage),
   },
   {
     path: 'create-exercise',
-    loadComponent: () => import('./exercises/create-exercise/create-exercise.page').then( m => m.CreateExercisePage)
+    loadComponent: () =>
+      import('./exercises/create-exercise/create-exercise.page').then(
+        (m) => m.CreateExercisePage
+      ),
   },
-
-
-
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./auth/register/register.page').then((m) => m.RegisterPage),
+  },
+  {
+    path: 'workout-detail/:id',
+    loadComponent: () =>
+      import('./layout/workout-detail/workout-detail.page').then(
+        (m) => m.WorkoutDetailPage
+      ),
+  },
 ];
