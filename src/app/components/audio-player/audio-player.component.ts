8ec40a0864e17ae84563b2f69dd2580c29e86d8c
Kévin <kevin.pasteur@icloud.com>
2024-01-28 02:36:37 +0100

    Update audio player component

--- src/app/components/audio-player/audio-player.component.ts
+++ src/app/components/audio-player/audio-player.component.ts
@@ -5,31 +5,75 @@ import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { play, pause } from 'ionicons/icons';
 import { addIcons } from 'ionicons';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   standalone: true,
   selector: 'app-audio-player',
   templateUrl: './audio-player.component.html',
   styleUrls: ['./audio-player.component.scss'],
-  imports: [CommonModule, IonicModule],
+  imports: [CommonModule, IonicModule, FormsModule],
 })
 export class AudioPlayerComponent implements OnDestroy {
   showPlayer = false;
   private playingSubscription: Subscription;
 
-  isPlaying = false;
+  currentTitle: string = '';
+  currentTime: number = 0;
+  duration: number = 0;
+
+  isPlaying = true;
 
   togglePlay() {
-    this.isPlaying = !this.isPlaying;
-    // Add logic to play or pause your audio
+    if (this.isPlaying) {
+      this.audioService.pause();
+    } else {
+      this.audioService.play();
+    }
   }
 
   constructor(private audioService: AudioService) {
+    this.showPlayer = false;
+    addIcons({ play, pause });
     this.playingSubscription = this.audioService.isPlaying$.subscribe(
       (isPlaying) => {
-        this.showPlayer = isPlaying;
+        this.isPlaying = isPlaying;
       }
     );
+
+    this.audioService.currentTitle$.subscribe((title) => {
+      this.currentTitle = title;
+    });
+
+    this.audioService.currentTime$.subscribe((time) => {
+      this.currentTime = time;
+    });
+
+    this.audioService.duration$.subscribe((newDuration) => {
+      this.duration = newDuration;
+    });
+
+    this.audioService.showPlayer$.subscribe((show) => {
+      this.showPlayer = show;
+    });
+  }
+
+  formatTime(time: number): string {
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+    const paddedSeconds = seconds < 10 ? `0${seconds}` : seconds;
+    return `${minutes}:${paddedSeconds}`;
+  }
+
+  onSliderChange(time: number) {
+    this.audioService.seekTo(time);
+    this.currentTime = time;
+    this.isPlaying = true;
+  }
+
+  closeAudioPlayer() {
+    this.audioService.stop();
+    this.showPlayer = false;
   }
 
   ngOnDestroy() {
