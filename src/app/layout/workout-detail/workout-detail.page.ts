ea6fc01c4f106874b8a8b06b57a7b652dcdef8cd
Kévin <kevin.pasteur@icloud.com>
2024-01-28 02:37:34 +0100

    Add css

--- src/app/layout/workout-detail/workout-detail.page.ts
+++ src/app/layout/workout-detail/workout-detail.page.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonicModule, ViewWillEnter, IonModal } from '@ionic/angular';
+import {
+  IonicModule,
+  ViewWillEnter,
+  IonModal,
+  ToastController,
+} from '@ionic/angular';
 import { WorkoutService } from 'src/app/services/workout.service';
 import { RouterLink, ActivatedRoute } from '@angular/router';
 import { switchMap } from 'rxjs/operators';
@@ -15,13 +20,22 @@ import {
 } from 'ionicons/icons';
 import { addIcons } from 'ionicons';
 import { Exercise } from 'src/app/exercises/create-exercise/exercise.model';
+import { AudioPlayerComponent } from 'src/app/components/audio-player/audio-player.component';
+import { AddExerciceComponent } from 'src/app/components/add-exercice/add-exercice.component';
+import { ModalController } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-workout-detail',
   templateUrl: './workout-detail.page.html',
   styleUrls: ['./workout-detail.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule, RouterLink],
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    RouterLink,
+    AudioPlayerComponent,
+  ],
 })
 export class WorkoutDetailPage implements ViewWillEnter {
   exercisesFromAWorkout: any[] = [];
@@ -29,6 +43,7 @@ export class WorkoutDetailPage implements ViewWillEnter {
   workout: any;
   averageLevel: string = '';
   averageDuration: string = '';
+  workoutId: any;
 
   @ViewChild('exerciseModal') exerciseModal: IonModal;
 
@@ -36,7 +51,9 @@ export class WorkoutDetailPage implements ViewWillEnter {
 
   constructor(
     private workoutService: WorkoutService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private modalCtrl: ModalController,
+    private toastController: ToastController
   ) {
     addIcons({
       heart,
@@ -51,11 +68,13 @@ export class WorkoutDetailPage implements ViewWillEnter {
     this.route.paramMap
       .pipe(
         switchMap((params) => {
-          const id = params.get('id');
+          this.workoutId = params.get('id');
 
           return forkJoin({
-            workout: this.workoutService.getWorkoutFromId$(id),
-            exercises: this.workoutService.getExercisesFromAWorkout$(id),
+            workout: this.workoutService.getWorkoutFromId$(this.workoutId),
+            exercises: this.workoutService.getExercisesFromAWorkout$(
+              this.workoutId
+            ),
           });
         })
       )
@@ -114,5 +133,72 @@ export class WorkoutDetailPage implements ViewWillEnter {
 
   stopEventPropagation(event: Event) {
     event.stopPropagation();
+  }
+
+  translateBodyPart(part: string): any {
+    const translations: { [key: string]: string } = {
+      pecs: 'Pectoraux',
+      shoulders: 'Épaules',
+      biceps: 'Biceps',
+      triceps: 'Triceps',
+      forearms: 'Avant-bras',
+      traps: 'Trapèzes',
+      back: 'Dos',
+      lowerback: 'Bas du dos',
+      glutes: 'Fessiers',
+      quadriceps: 'Quadriceps',
+      hamstrings: 'Ischio-jambiers',
+      calves: 'Mollets',
+    };
+    return translations[part] || part;
+  }
+
+  async openExerciseModal() {
+    this.workoutId;
+    const modal = await this.modalCtrl.create({
+      component: AddExerciceComponent,
+      componentProps: {
+        workoutId: this.workoutId,
+      },
+    });
+
+    modal.onDidDismiss().then(async (data) => {
+      if (data.data) {
+        const exercise = data.data;
+
+        this.refreshExercisesFromWorkout();
+
+        this.presentConfirmationToast(exercise.name);
+
+        const toast = await this.toastController.create({
+          message: `${exercise.name} a bien été ajouté à votre workout`,
+          duration: 2000,
+          color: 'success',
+        });
+        toast.present();
+      }
+    });
+
+    await modal.present();
+  }
+
+  refreshExercisesFromWorkout() {
+    this.workoutService
+      .getExercisesFromAWorkout$(this.workoutId)
+      .subscribe((exercises) => {
+        this.exercisesFromAWorkout = exercises;
+        // Calculez à nouveau les statistiques moyennes si nécessaire
+        this.averageLevel = this.calculateAverageLevel(exercises);
+      });
+  }
+
+  // Méthode pour afficher le toast de confirmation
+  async presentConfirmationToast(exerciseName: any) {
+    const toast = await this.toastController.create({
+      message: `${exerciseName} a bien été ajouté à votre workout`,
+      duration: 2000,
+      color: 'success',
+    });
+    toast.present();
   }
 }
