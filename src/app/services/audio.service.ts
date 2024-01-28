8006999665a42b522256990afc58c174d3b368c2
Kévin <kevin.pasteur@icloud.com>
2024-01-28 02:38:17 +0100

    Update audio service

--- src/app/services/audio.service.ts
+++ src/app/services/audio.service.ts
@@ -6,15 +6,58 @@ import { BehaviorSubject } from 'rxjs';
 })
 export class AudioService {
   private audio = new Audio();
-  private isPlaying = new BehaviorSubject<boolean>(true);
 
+  private isPlaying = new BehaviorSubject<boolean>(false);
   isPlaying$ = this.isPlaying.asObservable();
 
-  play(url: string = '') {
-    this.audio.src = '';
-    this.audio.load();
+  private currentTitle = new BehaviorSubject<string>('');
+  currentTitle$ = this.currentTitle.asObservable();
+
+  private currentTime = new BehaviorSubject<number>(0);
+  currentTime$ = this.currentTime.asObservable();
+
+  private duration = new BehaviorSubject<number>(0);
+  duration$ = this.duration.asObservable();
+
+  private showPlayer = new BehaviorSubject<boolean>(false);
+  showPlayer$ = this.showPlayer.asObservable();
+
+  constructor() {
+    this.audio.addEventListener('timeupdate', () => {
+      this.currentTime.next(this.audio.currentTime);
+    });
+
+    this.audio.addEventListener('loadedmetadata', async () => {
+      const audioContext = new window.AudioContext();
+      if (this.audio.duration === Infinity) {
+        const response = await fetch(this.audio.src);
+        const arrayBuffer = await response.arrayBuffer();
+        audioContext.decodeAudioData(arrayBuffer, ({ duration }) => {
+          this.duration.next(Math.floor(duration));
+        });
+      } else {
+        this.duration.next(Math.floor(this.audio.duration));
+      }
+    });
+
+    this.audio.addEventListener('ended', () => {
+      this.isPlaying.next(false); // Mettre à jour l'état de lecture
+    });
+  }
+
+  play(url?: string, title?: string) {
+    if (url && this.audio.src !== url) {
+      this.audio.src = url;
+      this.audio.load();
+    }
+
+    if (title) {
+      this.currentTitle.next(title);
+    }
+
     this.audio.play();
     this.isPlaying.next(true);
+    this.showPlayer.next(true);
   }
 
   pause() {
@@ -28,12 +71,14 @@ export class AudioService {
     this.isPlaying.next(false);
   }
 
-  updateProgress() {
-    this.audio.addEventListener('timeupdate', () => {
-      // Mettre à jour la progression
-      const currentTime = this.audio.currentTime;
-      const duration = this.audio.duration;
-      // Émettre la progression à l'aide d'un BehaviorSubject ou autre méthode
-    });
+  seekTo(time: number) {
+    this.audio.currentTime = time;
+    if (!this.isPlaying.getValue()) {
+      this.play(); // Lancer la lecture si elle est en pause
+    }
+  }
+
+  hidePlayer() {
+    this.showPlayer.next(false);
   }
 }
