a65a99aa3dbe27aca2df451e53f1505365997571
Kévin <kevin.pasteur@icloud.com>
2024-01-28 14:12:33 +0100

    update add exercise component to filter existing exercise

--- src/app/components/add-exercice/add-exercice.component.ts
+++ src/app/components/add-exercice/add-exercice.component.ts
@@ -7,6 +7,7 @@ import { CommonModule } from '@angular/common';
 import { AuthService } from 'src/app/security/auth.service';
 import { play, pause } from 'ionicons/icons';
 import { addIcons } from 'ionicons';
+import { WorkoutService } from 'src/app/services/workout.service';
 
 @Component({
   standalone: true,
@@ -20,10 +21,12 @@ export class AddExerciceComponent implements OnInit {
   filteredExercises: Exercise[] = [];
   searchTerm: string = '';
   currentUser: any;
+  selectedExerciseIds: string[] = [];
   @Input() workoutId: string;
 
   constructor(
     private exerciseService: ExerciseService,
+    private workoutService: WorkoutService,
     private modalCtrl: ModalController,
     private alertCtrl: AlertController,
     private authService: AuthService
@@ -35,8 +38,18 @@ export class AddExerciceComponent implements OnInit {
     this.exerciseService.exercises$.subscribe((exercises) => {
       this.exercises = exercises;
       console.log(exercises);
-      this.filteredExercises = [...this.exercises];
+      this.updateFilteredExercises();
     });
+
+    this.workoutService
+      .getExercisesFromAWorkout$(this.workoutId)
+      .subscribe((exercises) => {
+        this.selectedExerciseIds = exercises.map(
+          (exercise: any) => exercise.id
+        );
+        console.log(this.selectedExerciseIds);
+        this.updateFilteredExercises();
+      });
 
     this.authService.getUser$().subscribe((user) => {
       this.currentUser = user;
@@ -50,14 +63,24 @@ export class AddExerciceComponent implements OnInit {
     this.exerciseService.loadExercises(this.currentUser.id);
   }
 
+  updateFilteredExercises() {
+    this.filteredExercises = this.exercises.filter(
+      (exercise) => !this.selectedExerciseIds.includes(exercise.id)
+    );
+  }
+
   search(event: any) {
     const searchTerm = event.target.value.toLowerCase();
     if (searchTerm && searchTerm.trim() !== '') {
       this.filteredExercises = this.exercises.filter((exercise) => {
-        return exercise.name.toLowerCase().includes(searchTerm);
+        // Exclure les exercices déjà présents dans le workout
+        return (
+          !this.selectedExerciseIds.includes(exercise.id) &&
+          exercise.name.toLowerCase().includes(searchTerm)
+        );
       });
     } else {
-      this.filteredExercises = [...this.exercises];
+      this.updateFilteredExercises();
     }
   }
 
